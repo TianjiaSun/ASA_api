948da96f89c85d145db0e26a64a7a4b9c945b0e7
TianjiaSun <thomas.sun@outlook.com>
2015-02-19 20:07:35 -0800

    update also fill example into message

--- app/js/controller.js
+++ app/js/controller.js
@@ -23,11 +23,33 @@ apiApp.controller('ASA_API_Ctrl', function($scope, $http) {
   $scope.showInfo = function(app, index) {
       $scope.app=app;
       $scope.selected = index;
+      $scope.message = $scope.app.example; // assign the default message is the example
   }
   $scope.orderProp = 'customer_id';
 
   $scope.clearInfo = function(app) {
       $scope.app=$scope.initial;
   }
+
+  $scope.ASAsend = function(app, message) {
+    var req = {
+      method: 'POST',
+      url: 'http://asa.gausian.com',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      data: $.param({
+        user_app_id:'app_id',
+        service_app_name: $scope.app.app_name,
+        request_string: $scope.message
+      })
+    };
+    $http(req).success(function(data) {
+      console.log('Sent to ASA.');
+      console.log('ASA response_code is:');
+      console.log(data.response_code);
+      console.log('ASA response is:');
+      console.log(data.response);
+      $scope.asa_response = angular.fromJson(data.response);
+    });
+  };
 
 });
--- app/js/showcontrol.js
+++ app/js/showcontrol.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
   $("#dialog_container").hide();
+  $("#message_dialog").hide();
+  $("#response_dialog").hide();
+
   $( ".list_container" ).delegate( ".app_unit", "click", function() {
     $("#dialog_container").hide();
     $("#dialog_container").fadeIn(500);
+    $("#message_dialog").hide(); //clear previous message dialog from previous chatting
+    $("#response_dialog").hide(); //clear previous message dialog from previous chatting
   });
+
+  $("#bottom_send").click(function() {
+    $("#message_dialog").hide();
+    $("#message_dialog").fadeIn(200);
+    $("#response_dialog").hide();
+    $("#response_dialog").delay(200).fadeIn(200);
+    $("#dialog_container").delay(500).animate({scrollTop:$(document).height()}, 1000);
+  });
+
+  $(document).keypress(function(e){
+    if(e.keyCode==13)
+    $('#bottom_send').click();
+  });
+
 })
