469c28a59ffcb0a98f483b670bad19c6ad20c204
TianjiaSun <thomas.sun@outlook.com>
2015-02-28 18:31:46 -0800

    update

--- app/js/showcontrol.js
+++ app/js/showcontrol.js
@@ -15,7 +15,12 @@ $(document).ready(function() {
     $("#message_dialog").fadeIn(200);
     $("#response_dialog").hide();
     $("#response_dialog").delay(200).fadeIn(200);
-    $("#dialog_container").delay(500).animate({scrollTop:$(document).height()}, 1000);    
+    $("#dialog_container").delay(500).animate({scrollTop:$(document).height()}, 1000);   
+    $("#right_expend_button").show();
+    $("#code_container").hide();    
+    $("#copy_bn").hide();    
+    $("#right_dialog_container").css("width", "auto");
+    $("#right_dialog_container").css("height", "41px");
   });
 
   $(".list_container").click(function() {
@@ -23,16 +28,17 @@ $(document).ready(function() {
   });
 
   $("#right_expend_button").click(function() {
-      $("#right_dialog_container").animate({'width': '40vw'}, 500);
-      $("#right_dialog_container").delay(500).animate({'height': '200px'}, 500);
+      $("#right_dialog_container").animate({'width': '700px'}, 500);
+      $("#right_dialog_container").delay(0).animate({'height': '400px'}, 500);
       $("#right_expend_button").hide();
+      $("#code_container").show();    
+      $("#dialog_container").delay(200).animate({scrollTop:$(document).height()}, 1000);
+      $("#copy_bn").show();
   });
 
   $(document).keypress(function(e){
     if(e.keyCode==13)
     $('#bottom_send').click();
   });
-
-
 
 })
